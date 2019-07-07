9eff2b9f2af342d706bb22eb56329e43106e6153
Evert Bouw <bouw@outlook.com>
2019-07-07 12:24:04 +0200

    enhance autoban to act more like a spam filter

--- features/autoban.js
+++ features/autoban.js
@@ -1,16 +1,18 @@
 const autoban = {
-  tags: [
-    "http://discord.amazingsexdating.com",
-    "http://gambldiscord.bestoffersx.com",
-    "http://discordbetfaq.whatsappx.com/",
-    "http://discord.bestdatingforall.com/"
+  naughty: [
+    "dating", "naked", "pictures", "photos", "gambling", "betting", "whatsapp", "telegram"
   ],
-  handleMessage: ({ msg, user }) => {
-    let hasToken = false;
-    autoban.tags.forEach(token => {
-      if (msg.content.toLowerCase().includes(token)) hasToken = true;
-    });
-    if (hasToken) {
+  nice: ["react", "javascript", "hiring", "for hire"],
+  handleMessage: ({ msg }) => {
+    const messageContent = msg.content.toLowerCase();
+    const matchWord = word => messageContent.includes(word);
+
+    const isNewUser = (Date.now() - msg.author.createdTimestamp) < (48 * 3600 * 1000);
+    const containsLink = /https?:\/\//.test(messageContent);
+    const containsNaughtyWord = autoban.naughty.some(matchWord);
+    const containsNiceWord = autoban.nice.some(matchWord);
+
+    if (isNewUser && containsLink && containsNaughtyWord && !containsNiceWord) {
       msg.author.send(
         `Hello there! Our automated systems detected your message as a spam message and you have been banned from the server. If this is an error on our side, please feel free to contact one of the moderators.`
       );
