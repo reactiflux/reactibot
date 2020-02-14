2355d6431363bae276e2e8ef36ba9f1419eb5cf2
mlynchdev <56660298+mlynchdev@users.noreply.github.com>
2020-02-13 20:38:42 -0800

    Requested white space added.

--- features/commands.js
+++ features/commands.js
@@ -61,6 +61,7 @@ const commands = {
             title: "You Might Not Need Redux",
             type: "rich",
             description: `People often choose Redux before they need it. “What if our app doesn’t scale without it?
+
 https://medium.com/@dan_abramov/you-might-not-need-redux-be46360cf367`,
             color: 7506394
           }
@@ -77,6 +78,7 @@ https://medium.com/@dan_abramov/you-might-not-need-redux-be46360cf367`,
               "You might not need getDerivedStateFrom props or state at all!",
             type: "rich",
             description: `React 16.4 included a bugfix for getDerivedStateFromProps which caused some existing bugs in React components to reproduce more consistently. If this release exposed a case where your application was using an anti-pattern and didn’t work properly after the fix...
+
 https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html`,
             color: 7506394
           }
@@ -100,6 +102,7 @@ const handleEvent = e => {
 \`\`\`
 where \`state\` is not the most up to date value when you log it. This is caused by state updates being asynchronous.
 Check out these resources for more information:
+
 https://gist.github.com/bpas247/e177a772b293025e5324219d231cf32c
 https://reactjs.org/docs/state-and-lifecycle.html#state-updates-may-be-asynchronous`,
             color: 7506394
@@ -116,20 +119,25 @@ https://reactjs.org/docs/state-and-lifecycle.html#state-updates-may-be-asynchron
             title: "Binding functions",
             type: "rich",
             description: `In JavaScript, a class function will not be bound to the instance of the class, this is why you often see messages saying that you can't access something of undefined.
+
 In order to fix this, you need to bind your function, either in constructor:
+
 \`\`\`js
 class YourComponent extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick() {
     // you can access \`this\` here, because we've
     // bound the function in constructor
   }
 }
 \`\`\`
+
 or by using class properties babel plugin (it works in create-react-app by default!)
+
 \`\`\`js
 class YourComponent extends React.Component {
   handleClick = () => {
@@ -137,6 +145,7 @@ class YourComponent extends React.Component {
   }
 }
 \`\`\`
+
 Check out https://reactkungfu.com/2015/07/why-and-how-to-bind-methods-in-your-react-component-classes/ for more informations`,
             color: 7506394
           }
@@ -152,6 +161,7 @@ Check out https://reactkungfu.com/2015/07/why-and-how-to-bind-methods-in-your-re
             title: "Lifting State Up",
             type: "rich",
             description: `Often, several components need to reflect the same changing data. We recommend lifting the shared state up to their closest common ancestor. Let’s see how this works in action.
+
 https://reactjs.org/docs/lifting-state-up.html`,
             color: 7506394
           }
@@ -168,6 +178,7 @@ https://reactjs.org/docs/lifting-state-up.html`,
             title: "Asking to ask",
             type: "rich",
             description: `Instead of asking to ask, ask your question instead. People can help you better if they know your question.
+
 Bad: "hey can anyone help me?"	
 Bad: "anyone good with redux?"
 Good: 
@@ -191,12 +202,16 @@ Good:
             title: "Attaching Code",
             type: "rich",
             description: `When asking a question, try to include as much relevant code as you can.
+
 You can paste small bits of code directly in chat with syntax highlighting:
+
 \\\`\\\`\\\`js
 // your code goes here
 \\\`\\\`\\\`
+
 Or you can upload larger blocks of code (or files) to a service such as https://gist.github.com.
 We are also big fans of https://codesandbox.io/s/new where you can set up a working example of your code (or reproduce your bug). You can even link it to an existing GitHub repo so that others can try out your code live and help you solve your problems.
+
 `,
             color: 7506394
           }
@@ -212,6 +227,7 @@ We are also big fans of https://codesandbox.io/s/new where you can set up a work
             title: "Don’t ping or DM other devs you aren’t actively talking to",
             type: "rich",
             description: `It’s very tempting to try to get more attention to your question by @-mentioning one of the high profile(or recently active) members of Reactiflux, but please don’t. They may not actually be online, they may not be able to help, and they may be in a completely different timezone–nobody likes push notifications at 3am from an impatient stranger.
+
 Similarly, don’t DM other members without asking first. All of the same problems as @-mentioning apply, and private conversations can’t help anyone else. Your questions are likely not unique, and other people can learn from them when they’re kept public.`,
             color: 7506394
           }
