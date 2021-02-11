e7825cb00c75d2235c02ab80b81269bc113d866d
Robin Forbes <rsforbes0@gmail.com>
2021-02-11 16:37:00 +0000

    docs(sheduling): example message

--- src/features/scheduled-messages.ts
+++ src/features/scheduled-messages.ts
@@ -2,7 +2,26 @@ import { MessageOptions } from "child_process";
 import * as discord from "discord.js";
 import cron from "node-cron";
 
-export const MESSAGE_SCHEDULE: MessageConfig[] = [];
+export const MESSAGE_SCHEDULE: MessageConfig[] = [
+  /*  Example:
+  {
+    cronExpression: "0,30 * * * *",  // https://crontab.guru/#0,30_*_*_*_*
+    guilds: [
+      {
+        // getting these IDs: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
+        id: "102860784329052160",  // Reactiflux's server ID
+        channelIds: ["103696749012467712"]  // #help-react
+      }
+    ],
+    message: {
+      embed: {
+        title: "Example Message",
+        description: "This message is posted every 0th and 30th minute of the hour"
+      }
+    }
+  }
+  */
+];
 
 export type MessageConfig = {
   cronExpression: string;
