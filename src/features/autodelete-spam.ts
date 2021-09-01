a43e00cc3eb3ec9cd8fc71952e574e6916a9d71a
taranvohra <taran.vohra@hotmail.com>
2021-09-01 22:29:50 +0530

    log and autodelete msgs with here/everyone pings and spam keywords

---
+++ src/features/autodelete-spam.ts
@@ -0,0 +1,25 @@
+import { ChannelHandlers } from "../types";
+import { isStaff } from "../utils";
+
+const spamKeywords = ["discord", "nitro", "steam", "free", "gift"];
+
+const autodelete: ChannelHandlers = {
+  handleMessage: async ({ msg }) => {
+    if (isStaff(msg.member)) return;
+
+    const msgHasPingKeywords = ["@everyone", "@here"].some(pingKeyword =>
+      msg.content.includes(pingKeyword)
+    );
+
+    const msgHasSpamKeywords = msg.content
+      .split(" ")
+      .some(word => spamKeywords.includes(word.toLowerCase()));
+
+    if (msgHasPingKeywords && msgHasSpamKeywords) {
+      await msg.react("⚠️");
+      await msg.delete();
+    }
+  }
+};
+
+export default autodelete;
