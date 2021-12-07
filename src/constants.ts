11c552f8cef3ec9cf79b0b36cfa27e5f72366315
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-06 19:02:58 -0500

    Slight refactor of implementation
    Presume to post in Reactiflux, log to #bot-log, small naming/etc changes, add channel ID and frequency constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,13 @@ export const modRoleId = "&102870499406647296";
 
 export const staffRoles = ["mvp", "moderator", "admin", "admins"];
 
+export const enum CHANNELS {
+  "helpReact" = "103696749012467712",
+  "helpJs" = "565213527673929729",
+  "random" = "103325358643752960",
+  "jobBoard" = "103882387330457600",
+}
+
 export const enum ReportReasons {
   userWarn = "userWarn",
   userDelete = "userDelete",
--- src/features/scheduled-messages.ts
+++ src/features/scheduled-messages.ts
@@ -1,15 +1,28 @@
-import { MessageOptions } from "child_process";
-import * as discord from "discord.js";
+import type * as discord from "discord.js";
 import cron from "node-cron";
+import { logger } from "./log";
 
+// By keeping these at odd divisions, we can make sure they show up at all timezones. If it were */24, for instance, it would consistently show up in the middle of the night for some timezones.
+const enum FREQUENCY {
+  "often" = "0 */9 * * *",
+  "daily" = "0 */20 * * *",
+  "twiceWeekly" = "0 */68 * * *",
+  "weekly" = "0 */160 * * *",
+}
+
+type MessageConfig = {
+  cronExpression: string;
+  postTo: { guildId?: discord.Snowflake; channelId: discord.Snowflake }[];
+  message: discord.MessageOptions;
+};
 export const MESSAGE_SCHEDULE: MessageConfig[] = [
   /*  Example:
   {
     cronExpression: "0,30 * * * *",  // https://crontab.guru/#0,30_*_*_*_*
-    guilds: [
+    postTo: [
       {
         // getting these IDs: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
-        id: "102860784329052160",  // Reactiflux's server ID
+        id: "102860784329052160",  // Reactiflux's server ID, optional
         channelIds: ["103696749012467712"]  // #help-react
       }
     ],
@@ -54,25 +67,29 @@ const sendMessage = async (
   bot: discord.Client,
   messageConfig: MessageConfig,
 ) => {
-  for (let { id, channelIds } of messageConfig.guilds) {
-    const guild = await bot.guilds.fetch(id);
+  messageConfig.postTo.forEach(
+    async ({ guildId = "102860784329052160", channelId }) => {
+      const guild = await bot.guilds.fetch(guildId);
 
-    for (let channelId of channelIds) {
       const channel = guild.channels.resolve(channelId);
 
       if (channel === null) {
-        console.log(
-          `Failed to send a scheduled message: channel ${channelId} does not exist in guild ${id}.`,
+        logger.log(
+          "scheduled",
+          `Failed to send a scheduled message: channel ${channelId} does not exist in guild ${guildId}.`,
         );
-      } else if (!isTextChannel(channel)) {
-        console.log(
-          `Failed to send a scheduled message: channel ${channelId} in guild ${id} is not a text channel.`,
-        );
-      } else {
-        channel.send(messageConfig.message);
+        return;
       }
-    }
-  }
+      if (!isTextChannel(channel)) {
+        logger.log(
+          "scheduled",
+          `Failed to send a scheduled message: channel ${channelId} in guild ${guildId} is not a text channel.`,
+        );
+        return;
+      }
+      channel.send(messageConfig.message);
+    },
+  );
 };
 
 const isTextChannel = (
