eb485c12c4e463fa4140bd641de5f8005d4f150d
Nick McCurdy <nick@nickmccurdy.com>
2021-12-17 03:05:16 -0500

    Uninstall eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   env: {
     node: true,
