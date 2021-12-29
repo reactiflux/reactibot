f50ba374b516fe7c86066abf7028956e57375957
Carl Vitullo <carl.vitullo@gmail.com>
2021-12-29 16:08:27 -0600

    Clean up message contents

--- src/features/scheduled-messages.ts
+++ src/features/scheduled-messages.ts
@@ -5,8 +5,8 @@ import { scheduleTask } from "../helpers/schedule";
 
 const HOURLY = 60 * 60 * 1000;
 // By keeping these off 24 hr, we can make sure they show up at all timezones. If
-// it were */24, for instance, it would consistently show up in the middle of the
-// night for some timezones.
+// it were 24 hours, for instance, it would consistently show up in the middle of
+// the night for some timezones.
 const DAILY = 20 * HOURLY;
 const FREQUENCY = {
   often: 9 * HOURLY,
@@ -26,32 +26,30 @@ type MessageConfig = {
 const MESSAGE_SCHEDULE: MessageConfig[] = [
   /*  Example:
   {
-    interval: FREQUENCY.weekly
     // Find Discord channel IDs: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
     postTo: [
       {
         id: "102860784329052160",  // Reactiflux's server ID, optional
+        interval: FREQUENCY.weekly, // Frequency the bot should post by
         channelIds: [ CHANNELS.helpReact ]  // Add channel IDs to constants first!
       }
     ],
     message: {
-      embed: {
-        title: "Example Message",
-        description: "This message is posted every 0th and 30th minute of the hour"
-      }
+      content: "A message to post, any type of message discord.js understands"
     }
   }
   */
   {
     postTo: [{ interval: FREQUENCY.daily, channelId: CHANNELS.jobBoard }],
     message: {
-      content: `Messages must start with [FORHIRE] or [HIRING]. Lead with the location of the position and include LOCAL, REMOTE, INTERN, VISA, etc. and keep the message reasonably formatted & a reasonable length.
+      content: `Messages must start with [FORHIRE]/[HIRING]. Check the channel description for a full list of tags and rules!
 
-Please only post jobs once a week.
+* Job postings may only be posted every 7 days.
+* Posts should be reasonably descriptive.
+* Jobs are paid — unpaid, equity-only, or similar are not allowed.
+* We don't allow "small gigs".
 
-Jobs are paid. Unpaid jobs, one-off gigs, and equity-only positions may not be posted here.
-
-Job postings here do not go through an approval process. Please be diligent and approach with caution — if it doesn't feel right, it probably isn't. Protect yourself.
+Moderators may remove posts at any time, with or without warning. Repeat violators of these rules will be removed from the server permanently.
 `,
     },
   },
@@ -60,8 +58,8 @@ Job postings here do not go through an approval process. Please be diligent and 
     message: {
       content: `This channel is good for specific questions about syntax, debugging a small (< ~50 lines of code) snippet of JS, without React involved. Question not getting answered? Maybe it's hard to answer, check out these resources for how to ask a good question:
 
-How to ask for programming help http://wp.me/p2oIwo-26
-How do I ask a good question https://stackoverflow.com/help/how-to-ask
+How to ask for programming help <http://wp.me/p2oIwo-26>
+How do I ask a good question <https://stackoverflow.com/help/how-to-ask>
 `,
     },
   },
@@ -70,8 +68,8 @@ How do I ask a good question https://stackoverflow.com/help/how-to-ask
     message: {
       content: `This channel is good for specific questions about React, how React's features work, or debugging a small (< ~50 lines of code) snippet of JS that uses React. Question not getting answered? Maybe it's hard to answer, check out these resources for how to ask a good question:
 
-How to ask for programming help http://wp.me/p2oIwo-26
-How do I ask a good question https://stackoverflow.com/help/how-to-ask
+How to ask for programming help <http://wp.me/p2oIwo-26>
+How do I ask a good question <https://stackoverflow.com/help/how-to-ask>
 `,
     },
   },
@@ -90,12 +88,12 @@ How do I ask a good question https://stackoverflow.com/help/how-to-ask
 #general-tech Discussion of non-JS code, or that new laptop you're deciding on.
 #tooling for questions about building, linting, generating, or otherwise processing your code.
 
-Looking for work? Trying to hire? Check out #job-board, or <https://www.reactiflux.com/jobs>
+Looking for work? Trying to hire? Check out #job-board, or <https://reactiflux.com/jobs>
 
 Has someone been really helpful? Shoutout who and what in #thanks! We keep an eye in there as one way to find new MVPs. Give us all the reactions in there too!
 
-Please remember our Code of Conduct: <https://www.reactiflux.com/conduct>
-and our guidelines for promotion: <https://www.reactiflux.com/promotion>
+Please remember our Code of Conduct: <https://reactiflux.com/conduct>
+and our guidelines for promotion: <https://reactiflux.com/promotion>
 
 If you see anything that violates our rules, help alert the mods by reacting to it with 👎
 `,
@@ -106,7 +104,7 @@ If you see anything that violates our rules, help alert the mods by reacting to 
       { interval: FREQUENCY.moreThanWeekly, channelId: CHANNELS.random },
     ],
     message: {
-      content: `Have you read our Code of Conduct? <https://www.reactiflux.com/conduct> Is that joke you want to make really in keeping with it? Don't make dad angry.
+      content: `Have you read our Code of Conduct? <https://reactiflux.com/conduct> Don't make dad angry.
 
 If something crosses a line, give it a 👎, or if you'd prefer to remain anonymous, let mods know with the form at <https://reactiflux.com/contact>`,
     },
