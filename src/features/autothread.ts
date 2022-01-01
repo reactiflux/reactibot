2ef5be08f32532843212f6903ca0733d75a88e2e
Carl Vitullo <carl.vitullo@gmail.com>
2022-01-01 17:27:32 -0600

    Allow thread author to mark an "accepted answer"

--- src/features/autothread.ts
+++ src/features/autothread.ts
@@ -1,6 +1,43 @@
 import { format } from "date-fns";
+import { Message, PartialMessage } from "discord.js";
+import { constructDiscordLink } from "../helpers/discord";
 import { sleep } from "../helpers/misc";
 import { ChannelHandlers } from "../types";
+
+const CHECKS = ["☑️", "✔️", "✅"];
+
+const lockWithReply = ({
+  content: msg,
+  message,
+  starter,
+  shouldReply,
+}: {
+  content: string;
+  message: Message | PartialMessage;
+  starter: Message | PartialMessage;
+  shouldReply: boolean;
+}) => {
+  const { channel: thread } = message;
+  if (!thread.isThread()) {
+    return;
+  }
+
+  const content = `${msg}
+
+If you have a a followup question, you may reply to this thread so other members know they're related. ${constructDiscordLink(
+    starter,
+  )}
+
+Threads are closed automatically after 6 hours, or if the member who started it reacts to a message with ✅ to mark that as the accepted answer.`;
+
+  (shouldReply
+    ? message.reply({ content, allowedMentions: { repliedUser: false } })
+    : thread.send(content)
+  ).then(() => {
+    thread.setLocked(true);
+    thread.setArchived(true);
+  });
+};
 
 const autoThread: ChannelHandlers = {
   handleMessage: async ({ msg: maybeMessage }) => {
@@ -15,7 +52,7 @@ const autoThread: ChannelHandlers = {
       if (msg.author.id !== repliedTo.author.id) {
         msg.author.send(msg.content);
         const reply = await msg.reply(
-          "This is a thread-only channel! Please reply in that message’s thread. Your message has been DM'd to you.",
+          "This is a thread-only channel! Please reply in that message’s thread. Your message has been DM’d to you.",
         );
         msg.delete();
         sleep(5).then(() => reply.delete());
@@ -25,6 +62,26 @@ const autoThread: ChannelHandlers = {
     msg.startThread({
       name: `${msg.author.username} – ${format(new Date(), "HH-mm MMM d")}`,
     });
+  },
+  handleReaction: async ({ reaction }) => {
+    if (!CHECKS.includes(reaction.emoji.toString())) {
+      return;
+    }
+
+    const { channel: thread } = reaction.message;
+    const starter = thread.isThread()
+      ? await thread.fetchStarterMessage()
+      : undefined;
+
+    // If the reaction was sent by the original thread author, lock the thread
+    if (starter && reaction.users.cache.has(starter.author.id)) {
+      lockWithReply({
+        content: `This question has an answer! Thank you for helping 😄`,
+        message: reaction.message,
+        starter,
+        shouldReply: true,
+      });
+    }
   },
 };
 export default autoThread;
