39dfc4ecd5b145a8ebb8daa583278ae1f657387d
Carl Vitullo <vcarl@users.noreply.github.com>
2022-01-03 11:39:40 -0600

    Hotfix channel id for #help-threads-react (#150)

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const staffRoles = ["mvp", "moderator", "admin", "admins"];
 
 export const enum CHANNELS {
   "helpReact" = "103696749012467712",
-  "helpThreadsReact" = "565213527673929729",
+  "helpThreadsReact" = "902647189120118794",
   "helpJs" = "565213527673929729",
   "random" = "103325358643752960",
   "jobBoard" = "103882387330457600",
