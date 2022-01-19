6209c1decf519f85abf14e81ae8c8c79f3c5c473
Carl Vitullo <carl.vitullo@gmail.com>
2022-01-19 16:45:12 -0500

    Track incoming messages

--- src/features/jobs-moderation.ts
+++ src/features/jobs-moderation.ts
@@ -6,10 +6,7 @@ const storedMessages: Message[] = [];
 
 const DAYS_OF_POSTS = 7;
 
-const jobModeration = async (bot: Client) => {
-  const jobBoard = await bot.channels.fetch(CHANNELS.jobBoard);
-
-  if (!jobBoard?.isText()) return;
+const loadJobs = async (bot: Client, channel: TextChannel) => {
   const now = new Date();
 
   let oldestMessage: Message | undefined;
@@ -17,7 +14,7 @@ const jobModeration = async (bot: Client) => {
     !oldestMessage ||
     differenceInDays(now, oldestMessage.createdAt) < DAYS_OF_POSTS
   ) {
-    const newMessages = await jobBoard.messages.fetch({
+    const newMessages = await channel.messages.fetch({
       limit: 10,
       ...(oldestMessage ? { after: oldestMessage.id } : {}),
     });
@@ -28,11 +25,55 @@ const jobModeration = async (bot: Client) => {
 
     storedMessages.push(
       ...newMessages
-        .filter((m) => differenceInDays(now, m.createdAt) < DAYS_OF_POSTS)
+        .filter(
+          (m) =>
+            differenceInDays(now, m.createdAt) < DAYS_OF_POSTS &&
+            m.author.id !== bot.user?.id,
+        )
         .values(),
     );
   }
-  console.log("loaded all", storedMessages.length);
+};
+
+const updateJobs = (message: Message) => {
+  storedMessages.push(message);
+
+  // Remove all posts that are older than the limit
+  const now = new Date();
+  while (
+    storedMessages[0] &&
+    differenceInDays(now, storedMessages[0].createdAt) >= DAYS_OF_POSTS
+  ) {
+    storedMessages.shift();
+  }
+};
+
+const jobModeration = async (bot: Client) => {
+  const jobBoard = await bot.channels.fetch(CHANNELS.jobBoard);
+  if (!jobBoard?.isText() || !(jobBoard instanceof TextChannel)) return;
+
+  await loadJobs(bot, jobBoard);
+
+  bot.on("messageCreate", async (message) => {
+    if (
+      message.channelId !== CHANNELS.jobBoard ||
+      message.author.id === bot.user?.id
+    ) {
+      return;
+    }
+
+    const now = new Date();
+    const member = await message.guild?.members.fetch({
+      user: message.author.id,
+    });
+    const existingMessage = storedMessages.find(
+      (m) => m.author.id === message.author.id,
+    );
+    if (existingMessage) {
+      return;
+    }
+    updateJobs(message);
+  });
 };
 
 export default jobModeration;
