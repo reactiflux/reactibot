c37f7327daedc6b25b095bfd45b8c273334913a9
Carl Vitullo <carl.vitullo@gmail.com>
2022-03-22 02:42:07 -0400

    Build complete set of command types

--- scripts/deploy-commands.ts
+++ scripts/deploy-commands.ts
@@ -1,6 +1,9 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
+import {
+  ContextMenuCommandBuilder,
+  SlashCommandBuilder,
+} from "@discordjs/builders";
 import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
+import { ApplicationCommandType, Routes } from "discord-api-types/v9";
 import { applicationId, discordToken, guildId } from "../src/constants";
 import { logger } from "../src/features/log";
 
@@ -11,16 +14,41 @@ const cmd = {
 };
 const cmds = [cmd];
 
-const commands = cmds.map((c) =>
-  new SlashCommandBuilder()
-    .setName(c.name)
-    .setDescription(c.description)
-    .toJSON(),
-);
+const commands = [
+  ...cmds
+    .filter((x) => x.type === ApplicationCommandType.ChatInput)
+    .map((c) =>
+      new SlashCommandBuilder()
+        .setName(c.name)
+        .setDescription(c.description)
+        .toJSON(),
+    ),
+  ...cmds
+    .filter((x) => x.type === ApplicationCommandType.Message)
+    .map((c) =>
+      new ContextMenuCommandBuilder()
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error Discord.js doesn't export the union we need
+        .setType(ApplicationCommandType.Message)
+        .setName(c.name)
+        .toJSON(),
+    ),
+  ...cmds
+    .filter((x) => x.type === ApplicationCommandType.User)
+    .map((c) =>
+      new ContextMenuCommandBuilder()
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error Discord.js doesn't export the union we need
+        .setType(ApplicationCommandType.User)
+        .setName(c.name)
+        .toJSON(),
+    ),
+];
 
 const rest = new REST({ version: "9" }).setToken(discordToken);
 
 rest
+  // TODO: make this a global command in production
   .put(Routes.applicationGuildCommands(applicationId, guildId), {
     body: commands,
   })
