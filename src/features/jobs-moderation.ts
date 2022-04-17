f2037ef3b6b59a798ae2eaf89cfee12a6c332c70
Aldrin Mathew <admin@aldrinmathew.com>
2022-04-17 19:52:40 +0530

    Thread/Replies for warnings in job-board
    - The warnings posted by the bot in the job-board channel is now sent in
      a thread. If the thread creation fails the same warning is sent as a
      reply to the message in question

--- src/features/jobs-moderation.ts
+++ src/features/jobs-moderation.ts
@@ -165,19 +165,26 @@ const jobModeration = async (bot: Client) => {
       if (cooldown.hasCooldown(message.author.id, "user.jobs")) return;
 
       cooldown.addCooldown(message.author.id, "user.jobs");
-      message.author
-        .send(`Your post to #job-board didn’t have any tags - please consider adding some of the following tags to the start of your message to make your offer easier to find (and to index correctly on https://reactiflux.com/jobs):
-
-[FOR HIRE] - you are looking for a job
-[HIRING] - you are looking to hire someone
-[INTERN] - this is an intern position, no experience required
-[REMOTE] - only remote work is possible
-[LOCAL] - only local work is possible (please remember to provide the country / city!)
-[VISA] - Your company will help with the visa process in case of successful hire
-
-Thank you :)
-
-:robot: This message was sent by a bot, please do not respond to it - in case of additional questions / issues, please contact one of our mods!`);
+      const thread = await message.startThread({ name: 'Warning' });
+      const content = `Your post to #job-board didn't have any tags - please consider adding some of the following tags to the start of your message to make your offer easier to find (and to index correctly on https://reactiflux.com/jobs):
+  
+      [FOR HIRE] - you are looking for a job
+      [HIRING] - you are looking to hire someone
+      [INTERN] - this is an intern position, no experience required
+      [REMOTE] - only remote work is possible
+      [LOCAL] - only local work is possible (please remember to provide the country / city!)
+      [VISA] - Your company will help with the visa process in case of successful hire
+      
+      Thank you :)
+      
+      :robot: This message was sent by a bot, please do not respond to it - in case of additional questions / issues, please contact one of our mods!`;
+      if (thread) {
+        // Warning is sent in a newly created thread
+        thread.send(content);
+      } else {
+        // If thread creation fails, the warning is sent as a normal message
+        message.reply(content);
+      }
     }
 
     // Last, update the list of tracked messages.
