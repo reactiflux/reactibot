196256e9a088919f2a8e88c9cd4d2648a48b0542
Carl Vitullo <vcarl@users.noreply.github.com>
2022-07-30 12:54:49 -0400

    Fix env issues (#245)
    * Make some env fields optional
    
    * Fix env loading in command script
    
    Really this should just be changed to deploy on startup instead but that’s a lot more work

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -7,21 +7,21 @@ export const isProd = () => process.env.ENVIRONMENT === ENVIONMENTS.production;
 console.log("Running as", isProd() ? "PRODUCTION" : "TEST", "environment");
 
 let ok = true;
-const getEnv = (key: string) => {
+const getEnv = (key: string, optional = false) => {
   const value = process.env[key];
-  if (!value) {
+  if (!value && !optional) {
     console.log(`Add a ${key} value to .env`);
     ok = false;
     return "";
   }
-  return value;
+  return value ?? "";
 };
 
 export const applicationKey = getEnv("DISCORD_PUBLIC_KEY");
 export const applicationId = getEnv("DISCORD_APP_ID");
 export const guildId = getEnv("GUILD_ID");
 export const discordToken = getEnv("DISCORD_HASH");
-export const gitHubToken = getEnv("GITHUB_TOKEN");
-export const amplitudeKey = getEnv("AMPLITUDE_KEY");
+export const gitHubToken = getEnv("GITHUB_TOKEN", true);
+export const amplitudeKey = getEnv("AMPLITUDE_KEY", true);
 
 if (!ok) throw new Error("Environment misconfigured");
