d9eb826526233d93a720cd060930398e4eb641a4
gristoi <iangristock@gmail.com>
2022-09-24 19:02:44 +0100

    Adding a JWT command to help educate new devs on wether or not they n… (#254)
    * Adding a JWT command to help educate new devs on wether or not they need JWT in their authenticaiton flow. Have made it as neutral as possible to ensure they can see when its needed and not
    
    * Trimmed down initial message as per feedback, curated extra content via reactiflux community
    
    * Reworded the initial description based on the constructive feedback. Also reintroduced a small section on a brief description on scenarios when to use JWT
    
    Co-authored-by: ian gristock <ian.gristock@korelabs.co>

--- src/features/commands.ts
+++ src/features/commands.ts
@@ -568,6 +568,41 @@ Here's an article explaining the difference between the two: https://goshakkk.na
     },
   },
   {
+    words: [`!jwt`],
+    help: `Describes reasoning for and against the use of JWT tokens againt using standard sessions`,
+    category: "Reactiflux",
+    handleMessage: (msg) => {
+      msg.channel.send({
+        embeds: [
+          {
+            title:
+              "Is JWT the right approach for my applications authentication?",
+            type: "rich",
+            description: `
+            Most of the time, JWTs aren't the best approach for working with backend authentication, despite the multitude of tutorials that use JWT. Session have been used for decades, with a lot of back end frameworks supporting them out of the box.
+            
+            That said there are also scenarios when using a JWT token is the best approach:
+            - When using a third party auth service ( OpenID, Auth0, Firestore)
+            - Service to service calls
+            - Distributed architectures ( i.e Microservices)
+  
+            See below to help you decide which works best for you:
+
+            https://evertpot.com/jwt-is-a-bad-default
+            https://redis.com/blog/json-web-tokens-jwt-are-dangerous-for-user-sessions
+            https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html
+            https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html
+            https://cheatsheetseries.owasp.org/cheatsheets/JSON_Web_Token_for_Java_Cheat_Sheet.html
+            https://gist.github.com/samsch/a5c99b9faaac9f131967e8a6d61682b0
+
+            `,
+            color: EMBED_COLOR,
+          },
+        ],
+      });
+    },
+  },
+  {
     words: [`!formatting`, `!prettier`],
     help: `describes Prettier and explains how to use it to format code`,
     category: "Reactiflux",
