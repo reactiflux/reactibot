b33062ad70a3a70afa5a1ef692d3754dbde19940
Louis Young <me@louisyoung.co.uk>
2022-10-31 20:37:31 +0000

    #262: Update command blurb

--- src/features/commands.ts
+++ src/features/commands.ts
@@ -847,7 +847,9 @@ https://remotive.io/remote-jobs/software-dev
             title: "The importance of keys when rendering lists in React",
             type: "rich",
             description: `
-React depends on the use of stable and unique keys to identify items in a list so that it can perform correct and performant DOM updates. Keys are particularly important if the list can change over time.
+React depends on the use of stable and unique keys to identify items in a list so that it can perform correct and performant DOM updates. 
+
+Keys are particularly important if the list can change over time. React will use the index in the array by default if no key is specified. You can use the index in the array if the list doesn't change and you don't have a stable and unique key available. 
 
 Please see these resources for more information:
 
