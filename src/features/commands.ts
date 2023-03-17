030a111615709af1ec2b6bd09776b4ce2e247117
Daniel Gray <DanielFGray@gmail.com>
2023-03-17 15:48:55 -0500

    update !learn to point to official docs (#284)
    * update !learn to point to official docs
    
    beta docs are now stable
    
    * remove "old" react docs link from !learn

--- src/features/commands.ts
+++ src/features/commands.ts
@@ -690,11 +690,8 @@ To integrate it into your editor: https://prettier.io/docs/en/editors.html`,
             title: "Learning React",
             type: "rich",
             description: `
-          The official (beta) React docs are the best resource for learning React:
-          https://beta.reactjs.org
-
-          The official (stable) React docs still teach classes for the examples, but the concepts are still valid:
-          https://reactjs.org/docs/getting-started.html
+          The official React docs are the best resource for learning React:
+          https://react.dev
 
           The Reactiflux "Learning Resources" page has curated links for learning JS, React, Redux, and TS:
           https://www.reactiflux.com/learning
