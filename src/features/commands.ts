102cefb41a27f2ad941ed04226fbcb3c44f7381b
Daniel Gray <DanielFGray@gmail.com>
2023-03-17 15:42:16 -0500

    remove "old" react docs link from !learn

--- src/features/commands.ts
+++ src/features/commands.ts
@@ -689,9 +689,6 @@ To integrate it into your editor: https://prettier.io/docs/en/editors.html`,
           The official React docs are the best resource for learning React:
           https://react.dev
 
-          The official (stable) React docs still teach classes for the examples, but the concepts are still valid:
-          https://reactjs.org/docs/getting-started.html
-
           The Reactiflux "Learning Resources" page has curated links for learning JS, React, Redux, and TS:
           https://www.reactiflux.com/learning
           `,
