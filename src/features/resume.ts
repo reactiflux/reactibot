b76bd34d0dc7fb85285df3f230749b3bc98019ca
Carl Vitullo <carl.vitullo@gmail.com>
2024-06-10 19:14:17 -0400

    Link back to resume when logging feedback

--- src/features/resume.ts
+++ src/features/resume.ts
@@ -16,6 +16,7 @@ import {
   findResumeAttachment,
   REVIEW_COMMAND,
 } from "./resume-review";
+import { constructDiscordLink } from "../helpers/discord";
 
 const openai = new OpenAI({
   apiKey: openAiKey,
@@ -162,8 +163,11 @@ export const resumeResources = async (bot: Client) => {
         console.log({ content });
         const trimmed =
           content.at(0)?.slice(0, 2000) ?? "Oops! Something went wrong.";
-        logger.log("[RESUME]", `Feedback given:`);
-        logger.log("[RESUME]", trimmed);
+        logger.log(
+          "RESUME",
+          `Feedback given: ${constructDiscordLink(firstMessage)}`,
+        );
+        logger.log("RESUME", trimmed);
         deferred.edit({
           content: trimmed,
         });
