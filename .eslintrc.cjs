9f8485737d6ee932b60f06c5f818a03ca467a1e4
Carl Vitullo <carl.vitullo@gmail.com>
2024-12-30 22:03:49 -0500

    Modernized or whatever

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  parserOptions: { sourceType: "module" },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
