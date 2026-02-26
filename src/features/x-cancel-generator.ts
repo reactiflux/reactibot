8c652eaeb7c3ed4ec11453982e82f623d3861c7a
Asbed B <abb401@gmail.com>
2026-02-27 09:03:37 +1100

    supressed original message embed, moved converted link to a standard unfurled link

--- src/features/x-cancel-generator.ts
+++ src/features/x-cancel-generator.ts
@@ -1,5 +1,4 @@
 import type { ChannelHandlers } from "../types/index.js";
-import { EMBED_COLOR } from "./commands.js";
 
 export const TWITTER_REGEX =
   /https?:\/\/(?:www\.)?(?:x|twitter)\.com\/\w+\/status\/\d+/i;
@@ -8,22 +7,12 @@ const xCancelGenerator: ChannelHandlers = {
   handleMessage: async ({ msg }) => {
     const match = TWITTER_REGEX.exec(msg.content);
     if (!match || msg.author.bot) return; // Ignore bots to prevent loops
-
+    msg.suppressEmbeds(true);
     const [url] = match;
     const alternativeUrl = url.replace(/(x|twitter)\.com/i, "xcancel.com");
     await msg.channel.send(
-      "This `x.com` link has been converted to `xcancel.com` so that server members won't require an account to view content and threads:",
+      `This \`x.com\` link has been converted to \`xcancel.com\` so that server members won't require an account to view content and threads:\n ${alternativeUrl}`,
     );
-    await msg.channel.send({
-      embeds: [
-        {
-          title: "View on xCancel",
-          url: alternativeUrl,
-          description: `[Original Link](${url})`,
-          color: EMBED_COLOR,
-        },
-      ],
-    });
   },
 };
 
